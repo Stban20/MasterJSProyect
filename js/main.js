7e401bc278ed2ef2ad4891e69b22a696ee0fbd95
Stban20 <se.ag.2@hotmail.com>
2020-09-12 19:17:06 +0200

    Commit article list

--- js/main.js
+++ js/main.js
@@ -1,3 +1,61 @@
 $(document).ready(function () {
   console.log("Document is ready");
+
+  //------SideBar
+  $(".bxslider").bxSlider({
+    mode: "fade",
+    captions: true,
+    slideWidth: 1200,
+  });
+
+  //-----Post
+
+  let posts = [
+    {
+      title: "Title 1",
+      date: `Publish on: ${moment().format("LLL")}`,
+      content: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro
+      pariatur blanditiis sint! Et amet optio excepturi fugit blanditiis
+      alias facilis, distinctio vero ex rerum quae nihil quos facere
+      aliquam commodi`,
+    },
+    {
+      title: "Title 2",
+      date: `Publish on: ${moment().format("LLL")}`,
+      content: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro
+    pariatur blanditiis sint! Et amet optio excepturi fugit blanditiis
+    alias facilis, distinctio vero ex rerum quae nihil quos facere
+    aliquam commodi`,
+    },
+    {
+      title: "Title 3",
+      date: `Publish on: ${moment().format("LLL")}`,
+      content: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro
+      pariatur blanditiis sint! Et amet optio excepturi fugit blanditiis
+      alias facilis, distinctio vero ex rerum quae nihil quos facere
+      aliquam commodi`,
+    },
+    {
+      title: "Title 4",
+      date: `Publish on: ${moment().format("LLL")}`,
+      content: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro
+      pariatur blanditiis sint! Et amet optio excepturi fugit blanditiis
+      alias facilis, distinctio vero ex rerum quae nihil quos facere
+      aliquam commodi`,
+    },
+  ];
+
+  posts.forEach((elelment, index) => {
+    let post = `
+        <article class="articlePost">
+          <h2>${elelment.title}</h2>
+            <span class="spanDate">${elelment.date}</span>
+              <p>
+              ${elelment.content}
+              </p>
+            <a href="#" class="aButtonReadMore">Read More</a>
+      </article>   
+    `;
+    $("#divPosts").append(post);
+  });
 });
